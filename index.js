e79a6228e11b713c172abf263e6181d5b4b7dc23
ahmdfbryan <okangrian@gmail.com>
2026-09-23 16:18:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -65,13 +65,16 @@ client.on(Events.InteractionCreate, async (interaction) => {
       return;
     }
 
-    if (interaction.isModalSubmit() && interaction.customId === CHECK_ACCOUNT_MODAL_ID) {
+        if (interaction.isModalSubmit() && interaction.customId === CHECK_ACCOUNT_MODAL_ID) {
       const inputUsername = interaction.fields
         .getTextInputValue(CHECK_ACCOUNT_USERNAME_INPUT_ID)
         .trim();
 
+      const guildIconUrl = interaction.guild?.iconURL({ size: 128 }) || null;
+      const botAvatarUrl = interaction.client.user.displayAvatarURL({ size: 128 });
+
       await interaction.deferReply();
-      const embed = await checkEligibilityEmbed(inputUsername);
+      const embed = await checkEligibilityEmbed(inputUsername, { guildIconUrl, botAvatarUrl });
       await interaction.editReply({ embeds: [embed] });
       return;
     }
